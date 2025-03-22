8c0b308da51e15abcfabcf8fbbfd45fb252b10e4
baldiczxc <goracevvladislav2@gmail.com>
2025-03-22 23:53:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+#!/usr/bin/env python3
+
 import time
-import sqlite3
+import mysql.connector  # Используем MariaDB
+from mysql.connector import Error
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from threading import Thread  # Для параллельной обработки
+import configparser  # Для чтения конфигурационного файла
+import random
+import tempfile  # Для создания временных директорий
+import undetected_chromedriver as uc  # Импортируем undetected_chromedriver
+from selenium.webdriver.support.ui import WebDriverWait  # Для явного ожидания
+from selenium.webdriver.support import expected_conditions as EC  # Условия ожидания
 
 # Настройки Selenium
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Отключение обнаружения автоматизации
@@ -23,155 +33,227 @@ def load_links(file_path):
     with open(file_path, 'r') as file:
         return [line.strip() for line in file if line.strip()]
 
-# Подключение к локальной SQLite базе данных
+# Чтение конфигурации из config.txt
+def load_db_config(config_path='config.txt'):
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return {
+        'host': config.get('database', 'host'),
+        'user': config.get('database', 'user'),
+        'password': config.get('database', 'password'),
+        'database': config.get('database', 'database')
+    }
+
+# Подключение к MariaDB
 def init_db():
-    conn = sqlite3.connect('ozon_data.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp TEXT,
-            product_code TEXT,
-            price REAL,
-            card_price REAL,
-            original_price REAL,
-            price_change TEXT,
-            rating REAL,
-            questions_count INTEGER,
-            reviews_count INTEGER,
-            available INTEGER
+    try:
+        db_config = load_db_config()  # Загружаем конфигурацию
+        conn = mysql.connector.connect(
+            host=db_config['host'],
+            user=db_config['user'],
+            password=db_config['password'],
+            database=db_config['database']
         )
-    ''')
-    conn.commit()
-    return conn, cursor
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS ozon_price_parser (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                date DATETIME,  
+                timestamp BIGINT,  # UNIX-время
+                product_code VARCHAR(255),
+                price FLOAT,
+                card_price FLOAT,
+                original_price FLOAT,
+                price_change VARCHAR(255),
+                rating FLOAT,
+                rating_change INT, 
+                questions_count INT,
+                reviews_count INT,
+                available VARCHAR(255)
+            )
+        ''')
+        # Удалено создание таблицы products
+        conn.commit()
+        return conn, cursor
+    except Error as e:
+        print(f"Ошибка подключения к MariaDB: {e}")
+        exit(1)
+
+# Функция для получения данных по нескольким XPath
+def get_element(driver, xpaths, default=None, timeout=10):
+    for xpath in xpaths:
+        try:
+            # Явное ожидание появления элемента
+            element = WebDriverWait(driver, timeout).until(
+                EC.presence_of_element_located((By.XPATH, xpath))
+            )
+            return element.text.strip()
+        except Exception:
+            continue  # Пробуем следующий XPath
+    return default  # Возвращаем значение по умолчанию, если ни один XPath не сработал
 
 # Функция парсинга данных с одной страницы
 def parse_product_page(driver, article, cursor, conn):
     url = f"https://www.ozon.ru/product/{article}/"  # Формируем ссылку на основе артикула
     driver.get(url)
-    time.sleep(5)  # Увеличенная пауза для загрузки страницы
+    time.sleep(2)  # Короткая пауза для начальной загрузки страницы
 
     # Артикул товара
     product_code = article
 
     # Текущие дата и время
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = int(datetime.now().timestamp())
 
-    # Пример XPath (укажите свои)
+    # Пример XPath для разных вариантов отображения данных
     xpaths = {
-        'price': '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[1]/span[1]',
-        'card_price': '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]/button/span/div/div[1]/div/div/span',
-        'original_price': '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[1]/span[2]',  # Убрано /text()
-        'rating': '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div/div/div/div[2]/div[1]/a/div',
-        'questions_count': '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div/div/div/div[2]/div[2]/a/div',
-        'reviews_count': '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div/div/div/div[2]/div[1]/a/div',
-        'buy_button': '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[3]/div[1]/div[4]/div/div/div[1]/div/div/div/div[1]/button/div[2]'
+        'price': [
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[1]/span[1]',  # Основной блок
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[3]/div/div[1]/div/div/div[1]/div[1]/button/span/div/div[1]/div/div/span',  # Блок с распродажей
+        ],
+        'card_price': [
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]/button/span/div/div[1]/div/div/span',
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[3]/div/div[1]/div/div/div[1]/div[1]/button/span/div/div[1]/div/div/span',  # Альтернативный блок с картой
+        ],
+        'original_price': [
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[1]/span[2]',
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[2]/div/div/div[1]/div[3]/div/div[1]/div/div/div[1]/div[2]/div/div[1]/span[2]',  # Альтернативный блок с оригинальной ценой
+        ],
+        'rating': [
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div/div/div/div[2]/div[1]/a/div'
+        ],
+        'questions_count': [
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div/div/div/div[2]/div[2]/a/div'
+        ],
+        'reviews_count': [
+            '/html/body/div[1]/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div/div/div/div[2]/div[1]/a/div'
+        ],
     }
 
-    # Функция для получения данных по XPath
-    def get_element(xpath, default=None):
-        try:
-            element = driver.find_element(By.XPATH, xpath)
-            time.sleep(1)  # Задержка перед чтением текста элемента
-            return element.text.strip()
-        except NoSuchElementException:
-            return default  # Возвращаем значение по умолчанию, если элемент не найден
-
     # Собираем данные
-    price = get_element(xpaths['price'])
+    price = get_element(driver, xpaths['price'])
+    if not price:
+        print(f"Предупреждение: Цена не найдена для артикула {product_code}")
     price = float(price.replace('₽', '').replace('\u2009', '').replace('\xa0', '').replace(' ', '')) if price else None
 
-    card_price = get_element(xpaths['card_price'])
+    card_price = get_element(driver, xpaths['card_price'])
+    if not card_price:
+        print(f"Предупреждение: Цена с картой не найдена для артикула {product_code}")
     card_price = float(card_price.replace('₽', '').replace('\u2009', '').replace('\xa0', '').replace(' ', '')) if card_price else None
 
-    original_price = get_element(xpaths['original_price'])
+    original_price = get_element(driver, xpaths['original_price'])
+    if not original_price:
+        print(f"Предупреждение: Оригинальная цена не найдена для артикула {product_code}")
     original_price = float(original_price.replace('₽', '').replace('\u2009', '').replace('\xa0', '').replace(' ', '')) if original_price else None
 
-    rating = get_element(xpaths['rating'])
+    rating = get_element(driver, xpaths['rating'])
+    if not rating:
+        print(f"Предупреждение: Рейтинг не найден для артикула {product_code}")
     try:
         rating = float(rating.split('•')[0].strip()) if rating else None
     except (ValueError, AttributeError):
-        rating = None  # Устанавливаем значение None, если формат данных некорректен
+        rating = None
 
-    questions_count = get_element(xpaths['questions_count'])
+    questions_count = get_element(driver, xpaths['questions_count'])
+    if not questions_count:
+        print(f"Предупреждение: Количество вопросов не найдено для артикула {product_code}")
     questions_count = int(questions_count.split()[0]) if questions_count else 0
 
-    reviews_count = get_element(xpaths['reviews_count'])
+    reviews_count = get_element(driver, xpaths['reviews_count'])
+    if not reviews_count:
+        print(f"Предупреждение: Количество отзывов не найдено для артикула {product_code}")
     if reviews_count:
-        # Извлекаем только числовую часть из строки
+        reviews_count = reviews_count.split('•')[-1].strip()
         reviews_count = ''.join(filter(str.isdigit, reviews_count))
         reviews_count = int(reviews_count) if reviews_count else 0
     else:
         reviews_count = 0
 
     # Проверка наличия кнопки "Купить" на основе цены
-    available = "Доступен" if price is not None else "Нет в наличии"
+    available = 1 if price is not None else 0
 
     # Логика для "Стало дешевле"
-    price_change = "Без изменений"
+    price_change = 0
     if price is not None and original_price is not None:
-        cursor.execute('SELECT original_price FROM products WHERE product_code = ?', (product_code,))
-        previous_record = cursor.fetchone()
-        if previous_record:
-            previous_original_price = previous_record[0]
-            if previous_original_price is not None:  # Проверка на None
-                if price < previous_original_price:
-                    price_change = "Стало дешевле"
-                elif price > previous_original_price:
-                    price_change = "Стало дороже"
+        if price < original_price:
+            price_change = 1
+        elif price > original_price:
+            price_change = 1
+
+    # Логика для "Изменения рейтинга"
+    rating_change = 0  # По умолчанию без изменений
 
     # Вызов функции сохранения данных
-    save_to_db(cursor, conn, timestamp, product_code, price, card_price, original_price, 
-            price_change, rating, questions_count, reviews_count, available)
+    save_to_db(cursor, conn, date, timestamp, product_code, price, card_price, original_price, 
+            price_change, rating, rating_change, questions_count, reviews_count, available)
 
 # Запись или обновление данных в БД
-def save_to_db(cursor, conn, timestamp, product_code, price, card_price, original_price, 
-            price_change, rating, questions_count, reviews_count, available):
+def save_to_db(cursor, conn, date, timestamp, product_code, price, card_price, original_price, 
+            price_change, rating, rating_change, questions_count, reviews_count, available):
     # Проверяем, существует ли запись с таким product_code
-    cursor.execute('SELECT id FROM products WHERE product_code = ?', (product_code,))
+    cursor.execute('SELECT id FROM ozon_price_parser WHERE product_code = %s', (product_code,))
     existing_record = cursor.fetchone()
 
     if existing_record:
         # Обновляем существующую запись
         cursor.execute('''
-            UPDATE products
-            SET timestamp = ?, price = ?, card_price = ?, original_price = ?, 
-                price_change = ?, rating = ?, questions_count = ?, reviews_count = ?, available = ?
-            WHERE product_code = ?
-        ''', (timestamp, price, card_price, original_price, price_change, rating, 
-            questions_count, reviews_count, available, product_code))
+            UPDATE ozon_price_parser
+            SET date = %s, timestamp = %s, price = %s, card_price = %s, original_price = %s, 
+                price_change = %s, rating = %s, rating_change = %s, questions_count = %s, 
+                reviews_count = %s, available = %s
+            WHERE product_code = %s
+        ''', (date, timestamp, price, card_price, original_price, price_change, rating, 
+            rating_change, questions_count, reviews_count, available, product_code))
     else:
         # Вставляем новую запись
         cursor.execute('''
-            INSERT INTO products (timestamp, product_code, price, card_price, original_price, 
-                                price_change, rating, questions_count, reviews_count, available)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (timestamp, product_code, price, card_price, original_price, price_change, 
-            rating, questions_count, reviews_count, available))
+            INSERT INTO ozon_price_parser (date, timestamp, product_code, price, card_price, original_price, 
+                                        price_change, rating, rating_change, questions_count, 
+                                        reviews_count, available)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ''', (date, timestamp, product_code, price, card_price, original_price, price_change, 
+            rating, rating_change, questions_count, reviews_count, available))
     conn.commit()
+
+# Функция для обработки части артикулов
+def process_articles(articles):
+    conn, cursor = init_db()  # Создаём подключение к базе данных для каждого потока
+    temp_dir = tempfile.TemporaryDirectory()  # Создаём уникальную временную директорию для профиля браузера
+    try:
+        # Создаём драйвер с использованием undetected_chromedriver
+        options = uc.ChromeOptions()
+        options.add_argument(f"--user-data-dir={temp_dir.name}")  # Уникальная директория профиля
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-gpu")  # Отключение GPU для стабильности
+        options.add_argument("--window-size=1920,1080")  # Установка размера окна
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+        driver = uc.Chrome(options=options)
+
+        for article in articles:
+            print(f"Парсинг артикула: {article}")
+            try:
+                parse_product_page(driver, article, cursor, conn)
+            except Exception as e:
+                print(f"Ошибка при обработке артикула {article}: {e}")
+    finally:
+        driver.quit()  # Закрываем драйвер после завершения работы
+        temp_dir.cleanup()  # Удаляем временную директорию
+        conn.close()  # Закрываем подключение к базе данных
 
 # Основной цикл парсинга
 def main():
     file_path = 'SKU_Ozon.txt'  # Путь к вашему файлу с артикулами
     articles = load_links(file_path)
-    conn, cursor = init_db()
 
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    try:
-        start_time = datetime.now()  # Начало измерения времени
-        for article in articles:
-            print(f"Парсинг артикула: {article}")
-            parse_product_page(driver, article, cursor, conn)
-        end_time = datetime.now()  # Конец измерения времени
-        elapsed_time = end_time - start_time
-        print(f"Парсинг завершен. Затрачено времени: {elapsed_time.total_seconds() / 60:.2f} минут.")
-    finally:
-        driver.quit()
-        conn.close()
+    while True:  # Цикл для повторного парсинга
+        print("Запуск парсинга...")
+        process_articles(articles)  # Обрабатываем все артикулы в одном потоке
+        print("Ожидание следующего цикла...")
+        time.sleep(60)  # Задержка перед следующим кругом парсинга (например, 60 секунд)
 
 # Запуск как процесса с периодичностью (пример для Unix)
 if __name__ == "__main__":
     while True:
-        print("Запуск парсинга...")
         main()
-        print("Ожидание следующего цикла...")
